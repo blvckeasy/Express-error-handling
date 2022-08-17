abd7f4b0bc23e9561d982507645a34d218f3fdc6
blvckeasy <abdurakhmonovislom9@gmail.com>
2022-08-17 13:20:04 +0500

    express error handling successfully ended

--- src/server.js
+++ src/server.js
@@ -10,29 +10,32 @@ async function main() {
   try {
     const app = Express()
     const PORT = process.env.PORT || 8080
-    
+
     DotEnv.config()
-    
+
     app.use(Express.json())
 
     // set main path to routes
     await Routes(app)
-    
+
     // use route error handler
     app.use(apiErrorHandler)
 
     app.get('/', (req, res) => {
       res.json({
-          "Project name": "Error Handler",
-          "version": "1.0.0^development",
-          "author": "github.com/blvckeasy"
+        'Project name': 'Error Handler',
+        version: '1.0.0^development',
+        author: 'github.com/blvckeasy',
       })
-  })
+    })
 
-    app.listen(PORT, () => console.log(`🚀 SERVER READY @ http://localhost:${PORT}`))
+    
+    app.listen(PORT, () =>
+      console.log(`🚀 SERVER READY @ http://localhost:${PORT}`)
+    )
   } catch (error) {
     // if something wrong in the server it logs an error
-    console.log("SERVER_ERROR:", error);
+    console.log('SERVER_ERROR:', error)
   }
 }
 
